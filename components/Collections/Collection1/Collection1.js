31e5a691a5881bb7b541393f8fbc173f97fa1401
Chris Vargas <gqcodes@gmail.com>
2022-11-29 11:33:19 -0500

    add collection section markup

---
+++ components/Collections/Collection1/Collection1.js
@@ -0,0 +1,65 @@
+const Collection1 = () => {
+  return (
+    <div className='collection collection1'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <div className='collection1__collection-item'>
+              <div className='collection1__product-img'>
+                <img src='https://www.pngall.com/wp-content/uploads/2/Black-Sneakers.png' alt='' />
+              </div>
+              <div className='collection1__product-info'>
+                <div className='collection1__product-details'>
+                  <span className='collection1__product-title'>Sneakers</span>
+                  <span className='collection1__product-price'>Price</span>
+                </div>
+                <div className='collection1__colors'>
+                  <div className='collection__color collections1__color--blue'></div>
+                  <div className='collection__color collections1__color--white'></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-md-4'>
+            <div className='collection1__collection-item'>
+              <div className='collection1__product-img'>
+                <img src='https://www.pngall.com/wp-content/uploads/2/White-Sneakers-PNG-Clipart.png' alt='' />
+              </div>
+              <div className='collection1__product-info'>
+                <div className='collection1__product-details'>
+                  <span className='collection1__product-title'>Sneakers</span>
+                  <span className='collection1__product-price'>Price</span>
+                </div>
+                <div className='collection1__colors'>
+                  <div className='collection__color collections1__color--blue'></div>
+                  <div className='collection__color collections1__color--white'></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-md-4'>
+            <div className='collection1__collection-item'>
+              <div className='collection1__product-img'>
+                <img src='https://i.pinimg.com/originals/51/2b/4b/512b4b870390fac58da5206e88d228d6.png' alt='' />
+              </div>
+              <div className='collection1__product-info'>
+                <div className='collection1__product-details'>
+                  <span className='collection1__product-title'>Sneakers</span>
+                  <span className='collection1__product-price'>Price</span>
+                </div>
+                <div className='collection1__colors'>
+                  <div className='collection__color collections1__color--blue'></div>
+                  <div className='collection__color collections1__color--white'></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Collection1;
